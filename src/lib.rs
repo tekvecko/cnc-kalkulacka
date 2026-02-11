2d08c8138dcd38eed4eb763e2c088484beb79cb4
tekvecko <wrlockdj@gmail.com>
2026-02-11 16:04:02 +0100

    Sync everything to SDK 30

--- src/lib.rs
+++ src/lib.rs
@@ -1,29 +1,31 @@
 use eframe::egui;
-use android_activity::AndroidApp;
 
-#[no_mangle]
-fn android_main(app: AndroidApp) {
-    android_logger::init_once(android_logger::Config::default().with_max_level(log::LevelFilter::Info));
-    
-    let options = eframe::NativeOptions {
-        renderer: eframe::Renderer::Glow,
-        ..Default::default()
-    };
-    
-    eframe::run_native(
-        "CNC Reset",
-        options,
-        Box::new(|_cc| Box::new(MyApp)),
-    ).unwrap();
+pub struct CncApp {
+    label: String,
 }
 
-struct MyApp;
+impl Default for CncApp {
+    fn default() -> Self {
+        Self {
+            label: "Aplikace běží! (Verze 30)".to_owned(),
+        }
+    }
+}
 
-impl eframe::App for MyApp {
+impl eframe::App for CncApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
-            ui.heading("Funguju!");
-            ui.label("Verze Android SDK 31 načtena.");
+            ui.heading("CNC Kalkulačka");
+            ui.add_space(20.0);
+            ui.label(&self.label);
         });
     }
+}
+
+#[cfg(target_os = "android")]
+#[no_mangle]
+fn android_main(app: android_activity::AndroidApp) {
+    let mut options = eframe::NativeOptions::default();
+    options.renderer = eframe::Renderer::Glow;
+    eframe::run_native("CNC", options, Box::new(|_| Box::new(CncApp::default()))).unwrap();
 }
